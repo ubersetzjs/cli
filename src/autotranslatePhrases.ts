4f5c487c20df23d0fbb3d298736102932500df1d
Max Nowack <max@unsou.de>
2021-01-29 10:13:12 +0100

    Update locale after autotranslation

--- src/autotranslatePhrases.ts
+++ src/autotranslatePhrases.ts
@@ -12,18 +12,22 @@ const autotranslatePhrases = ({
   phrases: Record<string, string>,
   autotranslate: AutotranslationFunction,
 }) => new Observable((observer) => {
+  const { untranslated } = locale
   let count = 0
-  const update = () => observer.next(`${count}/${locale.untranslated.length} translated`)
+  const update = () => observer.next(`${count}/${untranslated.length} translated`)
   update()
 
   const promise = async () => {
-    await pMap(locale.untranslated, async (key) => {
+    await pMap(untranslated, async (key) => {
       const phrase = phrases[key]
       if (!phrase) throw new Error(`Cannot find phrase for key '${key}'`)
       const { text } = await autotranslate({
         text: phrase,
         targetLanguage: locale.code,
       })
+      // eslint-disable-next-line no-param-reassign
+      locale.untranslated = locale.untranslated.filter(i => i !== key)
+      locale.translated.push(key)
       count += 1
       update()
       await addPhraseToFile(locale.file, key, text)

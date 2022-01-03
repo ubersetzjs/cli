116a6aa15c921aa5d65fe3eb079d55ccebc6caa1
Max Nowack <max.nowack@gmail.com>
2022-01-03 12:17:31 +0100

    fix: use queue to add phrases to files

--- src/utils/addPhraseToFile.ts
+++ src/utils/addPhraseToFile.ts
@@ -1,11 +1,16 @@
+import PQueue from 'p-queue'
 import getPhrasesFromFile from './getPhrasesFromFile'
 import sortObject from './sortObject'
 import writeLocale from './writeLocale'
 
+const queue = new PQueue({ concurrency: 1 })
+
 const addPhraseToFile = async (file: string, key: string, text: string) => {
-  const localePhrases = await getPhrasesFromFile(file)
-  localePhrases[key] = text
-  await writeLocale(file, sortObject(localePhrases))
+  await queue.add(async () => {
+    const localePhrases = await getPhrasesFromFile(file)
+    localePhrases[key] = text
+    await writeLocale(file, sortObject(localePhrases))
+  })
 }
 
 export default addPhraseToFile

b5e9acb519343e398d2078e730e0bf5d51a9e751
Max Nowack <max.nowack@gmail.com>
2022-02-18 10:43:36 +0100

    fix: always sort locale keys

--- src/utils/addPhraseToFile.ts
+++ src/utils/addPhraseToFile.ts
@@ -1,6 +1,5 @@
 import PQueue from 'p-queue'
 import getPhrasesFromFile from './getPhrasesFromFile'
-import sortObject from './sortObject'
 import writeLocale from './writeLocale'
 
 const queue = new PQueue({ concurrency: 1 })
@@ -9,7 +8,7 @@ const addPhraseToFile = async (file: string, key: string, text: string) => {
   await queue.add(async () => {
     const localePhrases = await getPhrasesFromFile(file)
     localePhrases[key] = text
-    await writeLocale(file, sortObject(localePhrases))
+    await writeLocale(file, localePhrases)
   })
 }
 
--- src/utils/writeLocale.ts
+++ src/utils/writeLocale.ts
@@ -1,10 +1,11 @@
 import fs from 'fs-extra'
 import PQueue from 'p-queue'
+import sortObject from './sortObject'
 
 const queue = new PQueue({ concurrency: 1 })
 
 export default function writeLocale(file: string, localePhrases: Record<string, string>) {
-  return queue.add(() => fs.writeJSON(file, localePhrases, {
+  return queue.add(() => fs.writeJSON(file, sortObject(localePhrases), {
     spaces: 2,
   }))
 }
